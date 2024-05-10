c32da83f4f392f0e566a49e6929f63277cd07442
Allen Jacob Suarez Briseño <l21212056@tectijuana.edu.mx>
2024-05-09 20:42:48 -0700

    YASIRBE
    Sisirbe

--- ProyectoFinalLyA/trad.js
+++ ProyectoFinalLyA/trad.js
@@ -20,3 +20,34 @@ document.getElementById("textToBinBtn").addEventListener("click", function() {
 
     document.getElementById("outputBinary").value = textResult;
   });
+
+  // Obtener referencia al botón de ayuda
+var helpBtn = document.getElementById('helpBtn');
+
+// Agregar un evento de clic al botón de ayuda
+helpBtn.addEventListener('click', function() {
+  // Mostrar la ventana modal
+  showModal('ayuda.png');
+});
+
+// Función para mostrar la ventana modal con la imagen
+function showModal(imagePath) {
+  // Crear elemento de imagen
+  var img = document.createElement('img');
+  img.src = imagePath;
+
+  // Crear contenedor de la ventana modal
+  var modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // Agregar la imagen al contenedor modal
+  modal.appendChild(img);
+
+  // Agregar la ventana modal al cuerpo del documento
+  document.body.appendChild(modal);
+
+  // Agregar evento de clic al contenedor modal para cerrarlo al hacer clic fuera de él
+  modal.addEventListener('click', function() {
+    document.body.removeChild(modal);
+  });
+}
